92ad7aeda02ba83b98d4753ce7aa34abe6bfb639
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2020-12-04 17:04:12 +0530

    Initial Commit added intro

---
+++ intro.js
@@ -0,0 +1,12 @@
+/*
+Regular Expression
+
+Meta-chars
+
+single char             Quantifiers         position
+/d -> 0-9               * -> 0 or more      ^  -> beginning
+/w -> a-z A-z 0-9 _     + -> 1 or more      $  -> end
+/s -> whitespace        ? -> 0 or 1         /b -> word boundry
+.  -> any character     {min, max}
+                        {n}
+*/
